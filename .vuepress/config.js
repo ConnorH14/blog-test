eaeba08601ffbd545b28d57e8a9e4547ba127d46
ConnorH14 <ConnorH14@live.com>
2021-05-09 22:04:49 -0600

    confused

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports ={
   title: 'blog-test',
-  description : 'testing out blog'
+  description : 'testing out blog',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/'},
+      { text: 'Guide', link: '/guide/'},
+      { text: 'About Me', link: '/'},
+    ]
+  }
 }
